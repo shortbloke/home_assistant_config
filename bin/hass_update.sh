d62b8c344e2400c0fa7c0791500b25daab23cd12
shortbloke <martin@rowannet.co.uk>
2017-08-16 23:08:03 +0100

    Added script to update hass. Placeholder work begun for updating OS, however this won't work withour hass user being in sudoers list. Not sure I'm comfortable with that.

---
+++ bin/hass_update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /srv/hass/hass_venv/bin/activate
+pip3 install --upgrade homeassistant
---
+++ bin/os_update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove
